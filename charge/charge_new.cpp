a5c83972e41056c1994f982f7c6ba5f54bb95a07
SuperVerm <978692527@qq.com>
2022-12-03 19:29:48 +0800

    充电能量改进

---
+++ charge/charge_new.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int h, m, s, u, i, _h = 0, _m = 0, _s = 0, _u, _i;
+    char mao;
+    cin >> n;
+    int sum = 0;
+    for(int j = 0; j < n; j++)
+    {
+        cin >> h >> mao >> m >> mao >> s >> u >> i;
+        if(j > 0)
+        {
+        sum += ((h * 3600 + m * 60 + s) - (_h * 3600 + _m * 60 + _s)) * _u * _i;
+        }
+        _h = h;
+        _m = m;
+        _s = s;
+        _u = u;
+        _i = i;
+    }
+    cout << sum << endl;
+
+    system("pause");
+    return 0;
+}
